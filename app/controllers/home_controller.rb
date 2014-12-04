83170c1fd25bd92250161fca599ad393e12734c7
Javier Fonseca V <fonsecajavier@gmail.com>
2014-12-03 22:14:37 -0500

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,10 @@ class HomeController < ApplicationController
   end
 
   def random_color
-    color = "%06x" % (rand * 0xffffff)
+    color = "#%06x" % (rand * 0xffffff)
     puts "Received this from the client: #{params[:some_random_input]}"
     render json: {
-      color: "#%06x" % (rand * 0xffffff),
+      color: color,
       some_random_output: params[:some_random_input].to_f * 2
     }
   end
